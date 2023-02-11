ac0d4f18c617955b8f231426d337e7635b90b768
Stamatios Stamou Jr <ssjunior@ecentry.com>
2023-02-11 01:47:49 -0300

    routes added

--- easyapi/__init__.py
+++ easyapi/__init__.py
@@ -1,3 +1,4 @@
 from easyapi.base import BaseResource
 from easyapi.middleware import ExceptionMiddleware
 from easyapi.exception import HTTPException
+from easy.routes import get_routes
---
+++ easyapi/routes.py
@@ -0,0 +1,37 @@
+from django.urls import path, re_path
+from django.http import HttpResponse
+
+
+def get_route(route, view):
+    if hasattr(view, 'as_view'):
+        view = view.as_view()
+        return re_path(rf'{route}', view)
+
+    return path(route, view)
+
+
+def get_routes(endpoints):
+
+    def docs(request, *args, **kwargs):
+        for route, view in endpoints.items():
+            print(route, view)
+            print(view.model)
+            print(view.__dict__)
+            methods = view.allowed_methods
+            edit_fields = view.edit_fields
+            list_fields = view.edit_fields
+            update_fields = view.update_fields
+            print(methods, edit_fields, list_fields, update_fields)
+
+            model = view.model
+            if model:
+                print(model.__dict__)
+                print(view.fields)
+
+        return HttpResponse('Docs')
+
+    return [
+        get_route(key, value) for key, value in endpoints.items()
+    ] + [
+        path('docs', docs),
+    ]
